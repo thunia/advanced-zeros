93a9a0a0397cca083ef4c340d7ab859a57d2c629
Irina Kotova <irina.lushagina@gmail.com>
2019-03-24 11:27:48 +0300

    feat: getZerosCount function implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var primes = primeFactorsofNumber(base),
+      zerosCount = Number.MAX_SAFE_INTEGER;
+
+  for (var i = 0; i < primes.length; i++) {
+    zerosCount = Math.min(zerosCount, Math.floor(findPowerOfFactor(number, primes[i].prime) / primes[i].power));
+  }
+
+  return zerosCount;
+
+  function findPowerOfFactor(number, factor) {
+    var count = 0,
+        product = factor;
+    while (product <= number) {       
+      count += Math.floor(number / product);     
+      product *= factor;
+    }
+
+    return count;
+  }
+  
+  function primeFactorsofNumber(number) {
+    var primeFactors = [];
+
+    for (var i = 2; number != 1; i++) {
+      if (number % i == 0) {
+        var count = 0;
+
+        while (number % i == 0) {
+          number /= i;
+          count++;
+        }
+
+        primeFactors.push({ 'prime': i, 'power': count });
+      }
+    }
+    
+    return primeFactors;
+  }
 }
